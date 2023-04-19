42fe7faf019388c9c95cbc98cccb0f506fd851f0
Turner Bass <turner@astropanda.studio>
2023-04-18 22:52:55 -0500

    changing implementation of DefaultCachedRepository to take a dependency on IRepository

--- src/DefaultCachedRepository.cs
+++ src/DefaultCachedRepository.cs
@@ -4,22 +4,20 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 
 namespace BasicRepos;
 
-public class DefaultCachedRepository<TEntity, TContext> : RepositoryBase<TEntity>, ICachedRepository<TEntity>
-where TEntity : class
-where TContext : DbContext
+public class DefaultCachedRepository<TEntity> : ICachedRepository<TEntity> where TEntity : class
 {
+    private readonly IRepository<TEntity> _innerRepository;
     protected IEnumerable<TEntity> cachedEntities { get; set; } = new List<TEntity>();
 
-    public DefaultCachedRepository(TContext dbFactory) : base(dbFactory)
+    public DefaultCachedRepository(IRepository<TEntity> innerRepository)
     {
-    }
-    
+        _innerRepository = innerRepository;
+    }    
 
-    public override async Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
     {
         if (cachedEntities.Any() == false)
             await RefreshCache(cancellationToken);
@@ -27,7 +25,7 @@ where TContext : DbContext
         return cachedEntities.Where(predicate.Compile());
     }
 
-    public override async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
     {
         if (cachedEntities.Any() == false)
             await RefreshCache(cancellationToken);
@@ -35,7 +33,7 @@ where TContext : DbContext
         return cachedEntities;
     }
 
-    public override async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
+    public virtual async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
     {
         if (cachedEntities.Any() == false)
             await RefreshCache(cancellationToken);
@@ -43,7 +41,7 @@ where TContext : DbContext
         return cachedEntities.FirstOrDefault(predicate.Compile());
     }
 
-    public override async Task<bool> Exists(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
+    public virtual async Task<bool> Exists(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
     {
         if (cachedEntities.Any() == false)
             await RefreshCache(cancellationToken);
@@ -53,6 +51,70 @@ where TContext : DbContext
 
     public async Task RefreshCache(CancellationToken cancellationToken = default)
     {
-        cachedEntities = await base.GetAllAsync(cancellationToken);
+        cachedEntities = await _innerRepository.GetAllAsync(cancellationToken);
+    }
+
+    public virtual Task AddAsync(IEnumerable<TEntity> objects, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task UpdateAsync(IEnumerable<TEntity> objects, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task DeleteAsync(IEnumerable<TEntity> objects, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task ApplyChanges(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual IQueryable<TEntity> Query()
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> predicate)
+    {
+        if(cachedEntities.Any())
+            return cachedEntities.Where(predicate.Compile()).AsQueryable();
+
+        return _innerRepository.Query(predicate);
+    }
+
+    public virtual IQueryable<K> Query<K>() where K : TEntity
+    {
+        if(cachedEntities.Any())
+            return (cachedEntities as IEnumerable<K>).AsQueryable<K>();
+
+        return _innerRepository.Query<K>();
+    }
+
+    public virtual IQueryable RawQuery()
+    {
+        if(cachedEntities.Any())
+            return cachedEntities.AsQueryable();
+
+        return _innerRepository.RawQuery();
     }
 }
