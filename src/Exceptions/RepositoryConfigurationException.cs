b594dc8b9965006e9090314e8dfc808146931899
Turner Bass <turner@astropanda.studio>
2023-05-16 12:29:43 -0500

    Remove support for non-pooled DbContext instances handling for Cached Repositories; Add pre-validation for cached repository configuration when using non-pooled dbcontexts

---
+++ src/Exceptions/RepositoryConfigurationException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicRepos.Exceptions
+{
+    public class RepositoryConfigurationException : ConstraintException
+    {
+        public RepositoryConfigurationException()
+        {
+        }
+
+        public RepositoryConfigurationException(string s) : base(s)
+        {
+        }
+
+        public RepositoryConfigurationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected RepositoryConfigurationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
